ef23808bc214cf6375c859c593b4124fe2f8cb86
apmolina <apmolina@iu.edu>
2021-10-04 09:30:54 -0400

    Extra mechanics added

--- Molina RaB/Assets/Scripts/PlayerController.cs
+++ Molina RaB/Assets/Scripts/PlayerController.cs
@@ -17,6 +17,7 @@ public class PlayerController : MonoBehaviour
 
     private Vector3 scale = new Vector3(0.2f, 0.2f, 0.2f);
     private Vector3 checkPoint = new Vector3(0f, 0f, 0f);
+    private int checkPointDepth = -10;
 
     // Start is called before the first frame update
     void Start()
@@ -39,9 +40,6 @@ public class PlayerController : MonoBehaviour
 
     void SetCountText(){
         countText.text = "Count: " + count.ToString();
-        if(count >= 13){
-            winTextObject.SetActive(true);
-        }
     }
 
     void FixedUpdate(){
@@ -49,7 +47,7 @@ public class PlayerController : MonoBehaviour
 
         rb.AddForce(movement * speed);
 
-        if(gameObject.transform.position.y < -10){
+        if(gameObject.transform.position.y < checkPointDepth){
             gameObject.transform.position = checkPoint;
             rb.velocity = Vector3.zero;
         }
@@ -63,6 +61,25 @@ public class PlayerController : MonoBehaviour
             SetCountText();
 
             gameObject.transform.localScale += scale;
+        }
+
+        if(other.gameObject.CompareTag("Unpickup")){
+            other.gameObject.SetActive(false);
+            count++;
+
+            SetCountText();
+
+            gameObject.transform.localScale -= scale;
+        }
+
+        if(other.gameObject.CompareTag("Checkpoint")){
+            checkPoint = new Vector3(other.gameObject.transform.position.x, other.gameObject.transform.position.y + 5, other.gameObject.transform.position.z);
+            checkPointDepth -= 10;
+        }
+
+        if(other.gameObject.CompareTag("Goal")){
+            winTextObject.SetActive(true);
+            checkPointDepth = -1000;
         }
     }
 }
